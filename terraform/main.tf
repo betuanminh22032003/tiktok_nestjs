79068765b7b209363a776262a87a9021fbb4a62f
betuanminh <betuanminh22032003@gmail.com>
2025-12-28 21:35:19 +0700

    chore: Update CI workflows to allow manual triggers and optimize resource usage

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,31 +3,9 @@
 
 terraform {
   required_version = ">= 1.0"
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.10"
-    }
-  }
 
   backend "local" {
     path = "terraform.tfstate"
-  }
-}
-
-provider "kubernetes" {
-  config_path = var.kubeconfig_path
-  ctx         = var.kube_context
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = var.kubeconfig_path
-    ctx         = var.kube_context
   }
 }
 
