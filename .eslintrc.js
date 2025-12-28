905ef42328b26af3b9de2d6a9710a13f2156e8f6
betuanminh <betuanminh22032003@gmail.com>
2025-12-28 18:55:39 +0700

    fix: Change unused-imports rule severity from error to warn for better developer experience

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,9 +21,9 @@ module.exports = {
     // Remove default no-unused-vars from typescript-eslint to avoid conflicts
     '@typescript-eslint/no-unused-vars': 'off',
     // Use eslint-plugin-unused-imports to auto-remove unused imports/vars on fix
-    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-imports': 'warn',
     'unused-imports/no-unused-vars': [
-      'error',
+      'warn',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
   },
